a76412145f57a67b259243f4e68bb5560d0bdf18
anastasyah86 <nastasyah86@gmail.com>
2021-02-04 18:59:21 +0200

    typos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './styles.css';
 import fetchCountries from './js/fetchCountries';
-import renderMurkupCountry from './js/updateMarkupCountry';
-import renderMurkupCountries from './js/updateMarkupCountries';
+import renderMarkupCountry from './js/updateMarkupCountry';
+import renderMarkupCountries from './js/updateMarkupCountries';
 import refs from './js/refs';
 
 refs.input.addEventListener('input', event => {
   const inputCountry = event.target.value;
   refs.article.innerHtml = '';
 
-  fetchCountries(inputCountry).then(renderMurkupCountry).catch(console.log);
+  fetchCountries(inputCountry).then(renderMarkupCountry).catch(console.log);
 });
